e9f70d7a5fe2f59c093ca83a8291f2cb5fa2895b
deepak748030 <deepakkushwah748930@gmail.com>
2024-09-25 18:55:04 +0530

    update api secret

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -14,7 +14,7 @@ const fetchMatchData = async () => {
         method: 'GET',
         url: 'https://cricket-live-line1.p.rapidapi.com/upcomingMatches',
         headers: {
-            'x-rapidapi-key': 'b55e882438msh8961edaac57a08dp13336fjsn4e5a967292f7', // Replace with your API key
+            'x-rapidapi-key': 'd6a439bb1fmshd0b9d1b5a439a28p123e97jsn3b9786fc696b', // Replace with your API key
             'x-rapidapi-host': 'cricket-live-line1.p.rapidapi.com',
         },
     };
